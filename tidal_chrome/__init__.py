5de93cfb0753b6c2eb90e811033188feb0326634
Ben Blain <mail@servc.eu>
2018-01-12 02:32:21 +0000

    Read README from file

--- tidal_chrome/__init__.py
+++ tidal_chrome/__init__.py
@@ -1,9 +1,11 @@
+import os
+
 __version__ = '1.0'
 __description__ = 'Tidal-Chrome MPRIS bridge'
 requires = ['selenium', 'gi', 'dbus-python']
 
-README = """
-"""
+with open(os.path.join(os.path.dirname(__file__), '..', 'README.md')) as f:
+    README = f.read()
 
 # Tidal-Chrome MPRIS bridge
 
