c19397c8be1a4e86b39ee5aec6d6adb7d4f748ca
Ben Blain <mail@servc.eu>
2018-01-18 21:26:06 +0000

    Make introspectable

--- tidal_chrome/cli.py
+++ tidal_chrome/cli.py
@@ -40,6 +40,7 @@ class MPRIS(dbus.service.Object):
                                "CanSetFullscreen": True,
                                "CanRaise": False,
                                "HasTrackList": False,
+                               "DesktopEntry": "tidal-google-chrome",
                                "Identity": "Tidal-Chrome API bridge",
                                "SupportedUriSchemes": [''],
                                "SupportedMimeTypes": ['']}
@@ -126,6 +127,84 @@ class MPRIS(dbus.service.Object):
     def PropertiesChanged(self, interface_name, changed_properties,
                           invalidated_properties):
         pass
+
+    @dbus.service.method(dbus_interface=dbus.INTROSPECTABLE_IFACE,
+                         in_signature='', out_signature='s',
+                         path_keyword='object_path',
+                         connection_keyword='connection')
+    def Introspect(self, object_path, connection):
+        return """
+<node>
+    <interface name="org.freedesktop.DBus.Properties">
+        <method name="Get">
+            <arg type="s" name="interface_name" direction="in"/>
+            <arg type="s" name="property_name" direction="in"/>
+            <arg type="v" name="value" direction="out"/>
+        </method>
+        <method name="GetAll">
+            <arg type="s" name="interface_name" direction="in"/>
+            <arg type="a{sv}" name="properties" direction="out"/>
+        </method>
+        <method name="Set">
+            <arg type="s" name="interface_name" direction="in"/>
+            <arg type="s" name="property_name" direction="in"/>
+            <arg type="v" name="value" direction="in"/>
+        </method>
+        <signal name="PropertiesChanged">
+            <arg type="s" name="interface_name"/>
+            <arg type="a{sv}" name="changed_properties"/>
+            <arg type="as" name="invalidated_properties"/>
+        </signal>
+    </interface>
+    <interface name="org.mpris.MediaPlayer2">
+        <method name="Raise"/>
+        <method name="Quit"/>
+        <property type="b" name="CanQuit" access="read"/>
+        <property type="b" name="CanRaise" access="read"/>
+        <property type="b" name="HasTrackList" access="read"/>
+        <property type="s" name="Identity" access="read"/>
+        <property type="s" name="DesktopEntry" access="read"/>
+        <property type="as" name="SupportedUriSchemes" access="read"/>
+        <property type="as" name="SupportedMimeTypes" access="read"/>
+    </interface>
+    <interface name="org.mpris.MediaPlayer2.Player">
+        <method name="Next"/>
+        <method name="Previous"/>
+        <method name="Pause"/>
+        <method name="PlayPause"/>
+        <method name="Stop"/>
+        <method name="Play"/>
+        <method name="Seek">
+            <arg type="x" name="Offset" direction="in"/>
+        </method>
+        <method name="SetPosition">
+            <arg type="o" name="TrackId" direction="in"/>
+            <arg type="x" name="Position" direction="in"/>
+        </method>
+        <method name="OpenUri">
+            <arg type="s" name="Uri" direction="in"/>
+        </method>
+        <signal name="Seeked">
+            <arg type="x" name="Position"/>
+        </signal>
+        <property type="s" name="PlaybackStatus" access="read"/>
+        <property type="s" name="LoopStatus" access="readwrite"/>
+        <property type="d" name="Rate" access="readwrite"/>
+        <property type="b" name="Shuffle" access="readwrite"/>
+        <property type="a{sv}" name="Metadata" access="read"/>
+        <property type="d" name="Volume" access="readwrite"/>
+        <property type="x" name="Position" access="read"/>
+        <property type="d" name="MinimumRate" access="read"/>
+        <property type="d" name="MaximumRate" access="read"/>
+        <property type="b" name="CanGoNext" access="read"/>
+        <property type="b" name="CanGoPrevious" access="read"/>
+        <property type="b" name="CanPlay" access="read"/>
+        <property type="b" name="CanPause" access="read"/>
+        <property type="b" name="CanSeek" access="read"/>
+        <property type="b" name="CanControl" access="read"/>
+    </interface>
+</node>
+    """
 
     # Player controls
 
