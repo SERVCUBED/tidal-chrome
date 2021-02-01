9db5c3d759a2b8f26b3445160a97f811f74e06a7
Ben Blain <mail@servc.eu>
2021-02-01 15:45:58 +0000

    Move classes to separate files

--- tidal_chrome/__init__.py
+++ tidal_chrome/__init__.py
@@ -2,6 +2,10 @@ __version__ = '1.4'
 __description__ = 'Tidal-Chrome MPRIS bridge'
 requires = ['selenium', 'gobject', 'dbus-python']
 
+OPATH = "/org/mpris/MediaPlayer2"
+BASE_IFACE = "org.mpris.MediaPlayer2"
+PLAYER_IFACE = "org.mpris.MediaPlayer2.Player"
+BUS_NAME = "org.mpris.MediaPlayer2.tidal-chrome"
 
 # Tidal-Chrome MPRIS bridge
 
